900dc4df49741f2596f91074df897cd04ed5064b
Kenneth Izokun <kennetizokun@gmail.com>
2026-04-03 11:48:07 +0100

    all

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header";
+export { default as ContactCard } from "./ContactCard";
+export { default as BookList } from "./BookList";
+export { default as Book } from "./Book";
